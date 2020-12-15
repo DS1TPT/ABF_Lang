f95ddfb719f271ca3b6fc57a4b20c03b62455ffb
DS1TPT <49714844+DS1TPT@users.noreply.github.com>
2020-12-15 12:23:19 +0900

    Update ABF_Lang_Interpreter_dev15122020.c

--- ABF_Lang_Interpreter_dev15122020.c
+++ ABF_Lang_Interpreter_dev15122020.c
@@ -1,5 +1,5 @@
 /*
-* Copyright 2020 Lee Geon-goo
+* Copyright 2020. Lee Geon-goo
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
